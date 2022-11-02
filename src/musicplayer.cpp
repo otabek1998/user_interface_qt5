aba29ab3453716223800a0ecf2ed226b4455ccd7
otabek1998 <otabeknazrullaev1998@gmail.com>
2022-11-02 10:39:10 +0900

    Adding media playlist

--- src/musicplayer.cpp
+++ src/musicplayer.cpp
@@ -4,6 +4,7 @@
 #include <QDir>
 #include <QDirIterator>
 #include <QStringList>
+#include <QMediaPlaylist>
 
 MusicPlayer::MusicPlayer(QWidget *parent) : QWidget(parent)
 {
@@ -13,11 +14,14 @@ MusicPlayer::MusicPlayer(QWidget *parent) : QWidget(parent)
 void MusicPlayer::openFile()
 {
     QMediaPlayer player;
+    QMediaPlaylist playlist;
     QDirIterator it(*directoryOfUSB,QStringList() << "*.mp3",QDir::Files,QDirIterator::Subdirectories);
     while(it.hasNext())
     {
-        player.setMedia(QUrl(it.next()));
+        //player.setMedia(QUrl(it.next()));
         qDebug() << it.next();
+        playlist.addMedia(QUrl::fromLocalFile(it.next()));
     }
+    player.setPlaylist(&playlist);
     player.play();
 }
